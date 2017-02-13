d9d041c55530f69f4fcbb9f015b5a1f47d4677a1
wangyong <wangyong@DESKTOP-74GQ28E>
2017-02-13 17:29:58 +0800

    linux

---
+++ linuxagent.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Created on 2017年2月13日
+
+@author: wangyong
+'''
+
+import time   
+import urllib, urllib2
+import json
+
+class LinuxAgent():
+    '''
+    linux 采集数据
+    '''
+ 
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    def agentrun(self):
+        with open('/proc/meminfo') as f:
+            total = int(f.readline().split()[1])
+            free = int(f.readline().split()[1])
+            buffers = int(f.readline().split()[1])
+            cache = int(f.readline().split()[1])
+        mem_use = total - free - buffers - cache
+        data = {}
+        data['host'] = '192.168.245.132'
+        data['mem_total'] = total
+        data['mem_used'] = mem_use
+        data['mem_free'] = free
+        data['time'] = time.time()
+        print mem_use / 1024
+        return data
+        
+while True:
+    
+    la = LinuxAgent()
+    data = la.agentrun()  
+    
+    print u"内存使用率: " ,data,json.dumps(data)
+    req = urllib2.Request("http://192.168.245.36:8888/", json.dumps(data), {'Content-Type': 'application/json'})
+    f = urllib2.urlopen(req)
+    response = f.read()
+    print response
+    f.close()
+    time.sleep(5)
